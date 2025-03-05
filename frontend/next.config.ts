0e144d947e182ff95bfd4c4c72751978675d9c5b
WeiJian123-tech <Zhen16999@proton.me>
2025-03-05 18:22:10 -0500

    Update next.config.ts, package-lock.json, and package.json for static website export.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* Generate static page: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports */
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/SkatePave/' : '',
 };
 
 export default nextConfig;
