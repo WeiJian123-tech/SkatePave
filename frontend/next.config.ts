5219ab3e528ff7adeae4eed3ea0f4afeb82cb398
WeiJian123-tech <Zhen16999@proton.me>
2025-03-11 14:08:21 -0400

    Began adding navigation menu and content to homepage.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   /* Generate static page: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports */
   output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
   trailingSlash: true,
   basePath: process.env.NODE_ENV === 'production' ? '/SkatePave' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/SkatePave/' : '',
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-purple-500">
-      <h1>Welcome to Next.js!</h1>
-      <Image
-        src="/next.svg"
-        width={200}
-        height={200}
-        alt="Next.js logo"
-      />
+    <div className="bg-slate-900 w-screen h-screen">
+      <nav className="flex flex-row items-center justify-end align-middle w-full h-auto border border-indigo-600">
+        <a href="/next.svg">
+          <Image src="/" alt="logo" width={10} height={10} />
+        </a>
+
+        <button type="button">☰</button>
+      </nav>
+
+      <main>
+
+        <h1>Buy now!</h1>
+      </main>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
