2d2ee6ddb69e1f657662db5436fbad5eac9adbfe
WeiJian123-tech <Zhen16999@proton.me>
2025-03-31 11:50:55 -0400

    Added safety pads image. Added background color to product divs. Refactored dimensions and alignment of homepage elements to be better at responding and resizing according to device width.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -23,11 +23,11 @@ export default function Home() {
 
   return (
     <div className="bg-slate-900 w-screen h-screen overflow-y-auto">
-      <nav className="flex flex-col items-center w-full h-1/3 lg:h-1/6 border border-indigo-600">
+      <nav className="flex flex-col items-center w-full h-auto border border-indigo-600">
 
-        <div className="flex flex-row items-center justify-between w-full h-[18vh] lg:h-[16vh]">
+        <div className="flex flex-row items-center justify-between w-full h-auto lg:h-[10vh] xl:h-[8vh]">
 
-          <div className="flex flex-row items-center justify-start w-1/6 h-full">
+          <div className="flex flex-row items-center justify-start w-1/6 h-[5rem]">
             {/* Button that opens an overlay modal. In this instance it displays shop categories and deals. */}
             <button onClick={openOverlay} className="w-full h-full bg-blue-500 text-white rounded hover:bg-blue-400 active:bg-blue-400">
               ☰
@@ -40,7 +40,7 @@ export default function Home() {
           </div>
 
 
-          <div className="flex flex-row justify-center w-auto h-full">
+          <div className="flex flex-row justify-center w-auto h-full mx-4">
             <Link href="/" prefetch={true}>
               <Image src={getImagePath('SkatePaveLogoNH.png')} alt="logo" priority={true} width={128} height={128} className="w-auto h-full object-fill" />
             </Link>
@@ -59,8 +59,8 @@ export default function Home() {
 
         <div className="flex flex-row items-center justify-start w-full h-auto">
           <form className="w-full h-auto" name="searchForm">
-            <input type="search" name="searchbar" placeholder="Search..." aria-label="Search through site products" className="w-10/12 h-[15vh] lg:h-[7vh] m-0 rounded-sm bg-gray-500" />
-            <button type="button" name="searchBtn" className="w-2/12 h-[15vh] lg:h-[7vh] m-0 rounded-sm bg-green-500">🔎</button>
+            <input type="search" name="searchbar" placeholder="Search..." aria-label="Search through site products" className="w-10/12 lg:w-11/12 xl:w-23/24 h-[3rem] m-0 rounded-sm bg-gray-500" />
+            <button type="button" name="searchBtn" className="w-2/12 lg:w-1/12 xl:w-1/24 h-[3rem] m-0 rounded-sm bg-green-500">🔎</button>
           </form>
         </div>
 
@@ -74,10 +74,10 @@ export default function Home() {
           <h3 className="text-xl/snug italic">* For Select Items</h3>
         </div>
 
-        <div className="flex flex-col items-center justify-center w-full h-auto">
+        <div className="flex flex-col items-center justify-center w-full h-auto bg-amber-600">
           <div className="flex flex-row items-center justify-stretch w-full h-auto">
             <div className="flex flex-col items-center justify-center w-1/3 h-auto px-2">
-              <div className="w-full h-auto">
+              <div className=" flex flex-col items-center justify-centerw-full h-auto">
                 <h3>
                   Skateboards
                 </h3>
@@ -85,12 +85,12 @@ export default function Home() {
                   -20%
                 </h3>
               </div>
-              <div className="w-full h-auto">
+              <div className="flex items-center justify-center w-full h-auto">
                 <Image src={getImagePath('skateboardFront.png')} alt="skateboard image" width={128} height={128} className="h-auto object-fill" />
               </div>
             </div>
             <div className="flex flex-col items-center justify-center w-1/3 h-auto px-2">
-              <div className="w-full h-auto">
+              <div className="flex flex-col items-center justify-center w-full h-auto">
                 <h3>
                   Roller Skates
                 </h3>
@@ -98,12 +98,12 @@ export default function Home() {
                   -15%
                 </h3>
               </div>
-              <div className="w-full h-auto">
+              <div className="flex items-center justify-center w-full h-auto">
                 <Image src={getImagePath('RollerSkates.png')} alt="skateboard image" width={128} height={128} className="h-auto object-fill" />
               </div>
             </div>
             <div className="flex flex-col items-center justify-center w-1/3 h-auto px-2">
-              <div className="w-full h-auto">
+              <div className="flex flex-col items-center justify-center w-full h-auto">
                 <h3>
                   Bicycles
                 </h3>
@@ -111,26 +111,26 @@ export default function Home() {
                   -18%
                 </h3>
               </div>
-              <div className="w-full h-auto">
+              <div className="flex items-center justify-center w-full h-auto">
                 <Image src={getImagePath('BycicleDiagonal.png')} alt="skateboard image" width={128} height={128} className="h-auto object-fill" />
               </div>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-row items-center justify-center w-full text-center">
+        <div className="flex flex-col items-center justify-center w-full text-center bg-green-700">
           <h2>Save $7 dollars off these safety equipment!</h2>
+          <Image src={getImagePath('ElbowKneePads.png')} alt="Saftey pad equipment" width={128} height={128} className="h-auto object-fill" />
         </div>
 
-        <div className="flex flex-col items-center justify-center w-full text-center">
+        <div className="flex flex-col items-center justify-center w-full text-center bg-orange-800">
           <h2>Find awesome animal-style skateboards!</h2>
-          <button type="button" className="p-2 rounded bg-yellow-700 text-slate-300">Explore</button>
+          <button type="button" className="my-2 p-2 rounded bg-yellow-700 text-slate-300">Explore</button>
         </div>
       </main>
 
-      <footer className="w-full h-1/6">
-
-        <small className="text-center">@Copyright 2025</small>
+      <footer className="w-full h-auto text-center">
+        <small>© SkatePave 2025</small>
       </footer>
     </div>
   );
